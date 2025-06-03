86d0e2ebf09864bcfaa1c0f8d50443749445c7d2
ElighAlvarez <112344852+ElighAlvarez@users.noreply.github.com>
2025-06-03 00:36:16 -0500

    stats.py comments

--- stats.py
+++ stats.py
@@ -1,6 +1,8 @@
 def get_word_count(string):
     return len(string.split())
 
+# Determines the count of each character present in string
+# Returns a dict with char: count pairs
 def get_char_counts(string):
     lower = string.lower()
     counts = {}
@@ -11,6 +13,7 @@ def get_char_counts(string):
             counts[char] += 1
     return counts
 
+# Converts a dict with char: count pairs into a list of {"char": char, "count": count} entries
 def create_count_list_from_dict(char_counts):
     count_dicts = []
     for key in char_counts:
@@ -20,9 +23,12 @@ def create_count_list_from_dict(char_counts):
         })
     return count_dicts
 
+# Returns the value of "count" key from dict.
+# To be used for sorting a list of {"char": char, "count": count} entries
 def get_char_count_from_list_elem(char_list_elem):
     return char_list_elem["count"]
 
+# Returns a copy of char_counts sorted by descending "count" keys
 def sort_counts(char_counts):
     count_list = create_count_list_from_dict(char_counts)
     count_list.sort(key=get_char_count_from_list_elem, reverse=True)
